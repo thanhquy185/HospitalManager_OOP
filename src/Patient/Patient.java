7c35d613a543c811f2f46457d32db65e748afbf5
doduyquy <doduyquy211@gmail.com>
2024-10-16 09:23:51 +0700

    Not complete Patient, not understand all

--- src/Patient/Patient.java
+++ src/Patient/Patient.java
@@ -1,5 +1,37 @@
 package Patient;
 
-public class Patient {
+import Common.Person;
+
+public class Patient extends Person {
+    // Properties
+    private String ID;
+    private boolean isTest;
+    private String type;
+    private Relatives relatives;
+
+    // Constructors:
+    public Patient(){
+        super();
+    }
+    // Setter - Getter
+    public void setID(){
+
+    }
+    public void setType(String type){
+        this.type = type;   //Normal | Premium
+    }
+
+    public String getID(){
+        return this.ID;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public Relatives getRelatives(){
+        return this.relatives;
+    }
+
+
 
 }
+
