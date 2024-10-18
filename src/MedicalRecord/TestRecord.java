09686c78d278684b35ef63364c673406b5ac2a0c
khuuminhduc <110405kmd@gmail.com>
2024-10-18 21:25:41 +0700

    MinhDuc gui code

--- src/MedicalRecord/TestRecord.java
+++ src/MedicalRecord/TestRecord.java
@@ -1,5 +1,53 @@
 package MedicalRecord;
 
-public class TestRecord {
+    // Chứa thông tin về y tá, lý do và chẩn đoán
+public class TestRecord extends MedicalRecord {
+    //	Properties
+	private Nurse nurse;
+	private String reason;
+	private String diagnose;
 
+    // Constructor
+	public TestRecord() {
+		nurse = new Nurse();
+		reason = "";
+		diagnose = "";
+	}
+
+	public TestRecord(Nurse nurse, String reason, String diagnose) {
+		this.nurse = nurse;
+		this.reason = reason;
+		this.diagnose = diagnose;
+	}
+	public TestRecord(TestRecord testRecord)
+	{
+		this.nurse = new Nurse(testRecord.nurse);
+		this.reason = testRecord.reason;
+		this.diagnose = testRecord.diagnose;
+	}
+
+    // Setter - Getter
+	public void setNurse(Nurse nurse) {
+		this.nurse = nurse;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public void setDiagnose(String diagnose) {
+		this.diagnose = diagnose;
+	}
+
+	public Nurse getNurse() {
+		return this.nurse;
+	}
+
+	public String getReason() {
+		return this.reason;
+	}
+
+	public String getDiagnose() {
+		return this.diagnose;
+	}
 }
