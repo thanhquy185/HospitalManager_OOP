a45c2ca7a1491031a750c79502cc0747e76d7f22
khuuminhduc <110405kmd@gmail.com>
2024-10-18 21:27:55 +0700

    MinhDuc gui code

--- src/MedicalRecord/TreamentRecord.java
+++ src/MedicalRecord/TreamentRecord.java
@@ -1,5 +1,65 @@
 package MedicalRecord;
 
-public class TreamentRecord {
+    // Chứa thông tin về bệnh nhân (patient), bác sĩ (doctor), y tá (nurse), và tình trạng bệnh (sick).
+public class TreamentRecord extends MedicalRecord {
+    //	Properties
+	private Patient patient;
+	private Doctor doctor;
+	private Nurse nurse;
+	private Sick sick;
 
+    // Constructor
+	public TreamentRecord() {
+		this.patient = new Patient();
+		this.doctor = new Doctor();
+		nurse = new Nurse();
+		sick = new Sick();
+	}
+
+	public TreamentRecord(Patient patient, Doctor doctor, Nurse nurse, Sick sick) {
+		this.patient = patient;
+		this.doctor = doctor;
+		this.nurse = nurse;
+		this.sick = sick;
+	}
+
+		public TreamentRecord(TreamentRecord treamentRecord)
+		{
+			this.patient = new Patient(treamentRecord.patient);
+			this.doctor = new Doctor(treamentRecord.doctor);
+			this.nurse = new Nurse(treamentRecord.nurse);
+			this.sick = new Sick(treamentRecord.sick);
+		}
+    // Setter - Getter
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+
+	public void setNurse(Nurse nurse) {
+		this.nurse = nurse;
+	}
+
+	public void setSick(Sick sick) {
+		this.sick = sick;
+	}
+
+	public Patient getPatient() {
+		return this.patient;
+	}
+
+	public Doctor getDoctor() {
+		return this.doctor;
+	}
+
+	public Nurse getNurse() {
+		return this.nurse;
+	}
+
+	public Sick getSick() {
+		return this.nurse;
+	}
 }
